34b82c8de6958accfb72d292814ac1d4d58ce0ad
Amelia Arbisser <aarbisser@twitter.com>
2014-04-28 23:46:30 -0700

    stream xml, hashmaps, global stop

--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -1,69 +1,119 @@
+import collection.mutable.HashMap
 import scala.io.Source
+import scala.xml.pull._
 import scala.xml._
 
 object Search {
   type IndexEntry = (Int, Seq[Int])
-  type Index = Map[String, Seq[IndexEntry]]
+  type Index = HashMap[String, Seq[IndexEntry]]
+  type Page = (Int, String)
 
-  def getText(page: Node): String = {
-    val title = (page \\ "title").text
-    println(title)
-    title + (page \\ "text").text
-  }
+  val stop = Source.fromFile("src/main/resources/stop2.txt").getLines.toSet
+  val titleMap = new HashMap[Int, String]
 
-  def getId(page: Node): Int = {
-    (page \\ "id")(0).text.toInt
-  }
-
-  def getTerms(text: String, stop: Set[String]): Seq[String] = {
+  def getTerms(text: String): Seq[String] = {
     // TODO stem the terms here?
     text.split("\\s+")
       .map(_.toLowerCase)
       .filter(!stop.contains(_))
   }
 
-  def indexText(text: String, id: Int, stop: Set[String]): Index = {
-    val terms = getTerms(text, stop)
-    val occurrences = terms.zipWithIndex
+  def indexText(text: String, id: Int): Index = {
+    val terms = getTerms(text)
+    /* val occurrences = terms.zipWithIndex
     val group = occurrences.groupBy(_._1)
     group.mapValues { vals: Seq[(String, Int)] =>
       Seq((id, vals.map(_._2)))
+    } */
+
+    val index = new HashMap[String, Seq[(Int, Seq[Int])]]()
+    /* {
+      override def default(key: String) { Seq[(Int, Seq[Int])]((id, Seq[Int]())) }
+    } */
+    for ((word, position) <- terms.view.zipWithIndex) {
+      index(word) = if (index.contains(word)) {
+        Seq((id, index(word)(0)._2 ++ Seq(position)))
+      } else {
+        Seq((id, Seq(position)))
+      }
     }
+    index
   }
 
-  def combineIndices(right: Index, left: Index): Index = {
-    val indices = right.toSeq ++ left.toSeq
-    val grouped = indices.groupBy(_._1)
-    grouped.mapValues { vals: Seq[(String, Seq[IndexEntry])] =>
-      vals.map(_._2).flatMap { entry: Seq[IndexEntry] => entry }
+  def combineIndices(bigger: Index, smaller: Index): Index = {
+    for ((term, entries) <- smaller) {
+      bigger(term) = if (bigger.contains(term)) {
+        bigger(term) ++ entries
+      } else {
+        entries
+      }
     }
+    bigger
   }
 
-  def search(query: String, index: Index, stop: Set[String]): Seq[Int] = {
-    val terms = getTerms(query, stop)
+  def search(query: String, index: Index): Seq[Int] = {
+    val terms = getTerms(query)
     val term = terms(0)
     index.getOrElse(term, Seq()).map(_._1)
   }
 
-  def addPageToIndex(stop: Set[String])(index: Index, page: Node): Index = {
-    val id = getId(page)
-    val text = getText(page)
-    val pageIndex = indexText(text, id, stop)
-    
+  def addPageToIndex(index: Index, page: Page): Index = {
+    val (id, text) = page
+    val pageIndex = indexText(text, id)
     combineIndices(index, pageIndex)
   }
 
-  def main(args: Array[String]) = {
-    val stop = Source.fromFile("src/main/resources/stop2.txt").getLines.toSet
+  def parse(xml: XMLEventReader, index: Index) {
+    val page = new StringBuilder()
+    val id = new StringBuilder()
+    def loop(currNode: List[String]) {
+      if (xml.hasNext) {
+        xml.next match {
+          case EvElemStart(_, label, _, _) =>
+            loop(label :: currNode)
+          case EvElemEnd(_, label) =>
+            if (label == "page") {
+              addPageToIndex(index, (id.toString.trim.toInt, page.toString))
+              id.delete(0, id.length)
+              page.delete(0, page.length)
+            }
+            loop(currNode.tail)
+          case EvText(text) =>
+            if (currNode.length > 0 && currNode(0) == "text") {
+              page ++= " " + text
+            }
+            if (currNode.length > 0 && currNode(0) == "title") {
+              println(text)
+              page ++= " " + text
+            }
+            if (currNode.length > 1 && currNode(0) == "id" && currNode(1) == "page") {
+              id ++= text
+            }
+            loop(currNode)
+          case _ => loop(currNode)
+        }
+      }
+    }
+    loop(List.empty)
+  }
 
-    val anarchism = "src/main/resources/wiki100k.xml"
-    val source = XML.loadFile(anarchism)
-    val index = (source \\ "mediawiki" \\ "page").foldLeft(Map[String, Seq[(Int, Seq[Int])]]())(addPageToIndex(stop)(_, _))
+  def main(args: Array[String]) = {
+
+    val wikipedia = "src/main/resources/wiki100k.xml"
+    val source = Source.fromFile(wikipedia) // XML.loadFile(anarchism)
+    val index = new HashMap[String, Seq[(Int, Seq[Int])]]()
+
+    val xml = new XMLEventReader(source)
+    parse(xml, index)
 
     for (query <- args) {
-      println(search(query, index, stop))
+      println("Searching for " + query)
+      val ids = search(query, index)
+      for (id <- ids) {
+        println("\t" + id)
+      }
     }
 
-    println("done")
+    println("Done.")
   }
 }
